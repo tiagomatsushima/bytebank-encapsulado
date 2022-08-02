b470bd900cf53989e5d8659fcde59f56c852b94c
Tiago Matsushima <dev.matsu22@gmail.com>
2022-08-01 21:08:46 -0300

    encapsulamento

---
+++ src/TestaGetESet.java
@@ -0,0 +1,21 @@
+
+public class TestaGetESet {
+	
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta(1337, 24226);
+		
+		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		//conta.titular = paulo; não compila pq temos getter e setters
+		paulo.setNome("paulo silveira");
+		
+		conta.setTitular(paulo);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissão("programador");
+		
+	}
+}
